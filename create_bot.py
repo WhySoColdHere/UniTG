97d01040184fdf95a5fc96090422b52568e74713
WhereMySnow <lavrfan2005@mail.ru>
2023-11-01 22:29:37 +0300

    ThirdCommit

---
+++ create_bot.py
@@ -0,0 +1,13 @@
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from token_file import token
+
+storage = MemoryStorage()
+
+bot = Bot(token=token)
+ADMINS_CHAT_ID = -817203032
+
+dp = Dispatcher(bot, storage=storage)
+dp.middleware.setup(LoggingMiddleware())
