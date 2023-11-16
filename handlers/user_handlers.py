54959bff980559c1dcf9db46586e962c85fd794b
WhereMySnow <lavrfan2005@mail.ru>
2023-11-16 14:39:36 +0300

    NinethCommit

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -2,7 +2,7 @@ from aiogram import types
 from keyboards import common_kb, common_keyboard_names
 from create_bot import bot, dp
 from aiogram.dispatcher import FSMContext
-from states.schedule_states import ScheduleStates
+from states.schedule_states import ScheduleStatesStudents
 
 DATA = []
 
@@ -30,64 +30,79 @@ async def role_schedule_st(message: types.Message, state: FSMContext):
     await bot.send_message(message.chat.id, 'Выбери роль',
                            reply_markup=common_kb.group_keyboard_common_reply(
                                common_keyboard_names.roles()))
-    await state.set_state(ScheduleStates.waiting_for_role.state)
+    await state.set_state(ScheduleStatesStudents.waiting_for_role.state)
 
 
-@dp.message_handler(state=ScheduleStates.waiting_for_role)
+@dp.message_handler(state=ScheduleStatesStudents.waiting_for_role)
 async def institute_schedule_st(message: types.Message, state: FSMContext):
     await kinda_validator_st(message, common_keyboard_names.roles(), "Выберите существующую роль!")
     await state.update_data(role_st=message.text)
     DATA.append(message.text)
 
+    await bot.send_message(message.chat.id, 'Выбери уровень подготовки',
+                           reply_markup=common_kb.group_keyboard_common_reply(
+                               common_keyboard_names.levels_of_preparation()))
+    await state.set_state(ScheduleStatesStudents.waiting_for_level_of_preparation.state)
+
+
+@dp.message_handler(state=ScheduleStatesStudents.waiting_for_level_of_preparation)
+async def course_schedule_st(message: types.Message, state: FSMContext):
+    await kinda_validator_st(message, common_keyboard_names.levels_of_preparation(), "Выберите уровень подготовки!")
+    await state.update_data(level_of_preparation_st=message.text)
+    DATA.append(message.text)
+
     await bot.send_message(message.chat.id, 'Выбери институт',
                            reply_markup=common_kb.group_keyboard_common_reply(
                                common_keyboard_names.institutes()))
-    await state.set_state(ScheduleStates.waiting_for_institute.state)
+    await state.set_state(ScheduleStatesStudents.waiting_for_institute.state)
 
 
-@dp.message_handler(state=ScheduleStates.waiting_for_institute)
-async def course_schedule_st(message: types.Message, state: FSMContext):
+@dp.message_handler(state=ScheduleStatesStudents.waiting_for_institute)
+async def education_form_schedule_st(message: types.Message, state: FSMContext):
     await kinda_validator_st(message, common_keyboard_names.institutes(), "Выберите существующий институт!")
-    await state.update_data(institute_st=message.text)
+    await state.update_data(course_st=message.text)
     DATA.append(message.text)
 
     await bot.send_message(message.chat.id, 'Выбери курс',
                            reply_markup=common_kb.group_keyboard_common_reply(
                                common_keyboard_names.courses()))
-    await state.set_state(ScheduleStates.waiting_for_course.state)
+    await state.set_state(ScheduleStatesStudents.waiting_for_course.state)
 
 
-@dp.message_handler(state=ScheduleStates.waiting_for_course)
-async def education_form_schedule_st(message: types.Message, state: FSMContext):
-    await kinda_validator_st(message, common_keyboard_names.courses(), "Выберите существующий курс!")
-    await state.update_data(course_st=message.text)
+@dp.message_handler(state=ScheduleStatesStudents.waiting_for_course)
+async def group_schedule_st(message: types.Message, state: FSMContext):
+    await kinda_validator_st(message, common_keyboard_names.courses(),
+                             "Выберите существующий курс!")
+    await state.update_data(educational_form_st=message.text)
     DATA.append(message.text)
 
     await bot.send_message(message.chat.id, 'Выбери форму обучения',
                            reply_markup=common_kb.group_keyboard_common_reply(
                                common_keyboard_names.education_forms()))
-    await state.set_state(ScheduleStates.waiting_for_educational_form.state)
+    await state.set_state(ScheduleStatesStudents.waiting_for_educational_form.state)
 
 
-@dp.message_handler(state=ScheduleStates.waiting_for_educational_form)
-async def group_schedule_st(message: types.Message, state: FSMContext):
-    await kinda_validator_st(message, common_keyboard_names.education_forms(),
-                             "Выберите существующую форму обучения!")
-    await state.update_data(educational_form_st=message.text)
+@dp.message_handler(state=ScheduleStatesStudents.waiting_for_educational_form)
+async def final_schedule_st(message: types.Message, state: FSMContext):
+    await kinda_validator_st(message, common_keyboard_names.education_forms(), "Выбери существующую форму обучения!")
+    await state.update_data(group_st=message.text)
     DATA.append(message.text)
 
     await bot.send_message(message.chat.id, 'Выбери группу',
                            reply_markup=common_kb.group_keyboard_common_reply(
                                common_keyboard_names.groups()))
-    await state.set_state(ScheduleStates.waiting_for_group.state)
+    await state.set_state(ScheduleStatesStudents.waiting_for_group.state)
 
 
-@dp.message_handler(state=ScheduleStates.waiting_for_group)
+@dp.message_handler(state=ScheduleStatesStudents.waiting_for_group)
 async def final_schedule_st(message: types.Message, state: FSMContext):
     await kinda_validator_st(message, common_keyboard_names.groups(), "Выбери существующую группу!")
     await state.update_data(group_st=message.text)
     DATA.append(message.text)
 
     await bot.send_message(message.chat.id, DATA)
-
     # Сохраняем куда-нибудь запрос из DATA, дабы потом многократно к нему обращаться.
+
+    # Тактика такая: У нас статичен только список институтов.
+    # Как только мы его получаем, сразу обращаемся к бд, и выводим остальную инфу, на основе предыдущего 'узла', тк
+    # в их расписании все связано.
--- states/schedule_states.py
+++ states/schedule_states.py
@@ -1,12 +1,17 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
-class ScheduleStates(StatesGroup):
+class ScheduleStatesStudents(StatesGroup):
     waiting_for_role = State()
     waiting_for_institute = State()
+    waiting_for_level_of_preparation = State()
     waiting_for_course = State()
     waiting_for_educational_form = State()
     waiting_for_group = State()
 
-# Институт --> Курс --> Форма обучения (оч/заоч/оч-заоч) --> Выбор группы.
+
+class ScheduleStatesTeachers(StatesGroup):
+    pass
+
+# Институт --> уровень подготовки --> Курс --> Форма обучения (оч/заоч/оч-заоч) --> Выбор группы.
 # Парсер.
